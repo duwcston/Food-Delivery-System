eea9823182ddaee20b0ade43658bf6f62ef5ee3f
Đoàn Nhân Hậu <125809343+Tobi3007@users.noreply.github.com>
2023-02-26 10:05:59 +0700

    Add files via upload

---
+++ Điền Thông tin vào Bảng OrderReport.sql
@@ -0,0 +1,23 @@
+Insert dbo.OrderReport
+      ( OrderId, CustomerId, FoodId, DeliveryId)
+Values (1 , 1, 1, 1) ;
+
+Insert dbo.OrderReport
+      ( OrderId, CustomerId, FoodId, DeliveryId)
+Values (2 , 2, 2, 2) ;
+
+Insert dbo.OrderReport
+      ( OrderId, CustomerId, FoodId, DeliveryId)
+Values (3 , 3, 3, 3) ;
+
+Insert dbo.OrderReport
+      ( OrderId, CustomerId, FoodId,DeliveryId)
+Values (4 , 4, 4, 4) ;
+
+Insert dbo.OrderReport
+      ( OrderId, CustomerId, FoodId, DeliveryId)
+Values (5 , 5, 5, 5) ;
+
+Insert dbo.OrderReport
+      ( OrderId, CustomerId, FoodId, DeliveryId)
+Values (6 , 6, 6, 6) ;
---
+++ Điền thông tin vào Bảng Customer.sql
@@ -0,0 +1,23 @@
+Insert dbo.Customer
+      ( CustomerId)
+Values (1) ;
+
+Insert dbo.Customer
+      ( CustomerId)
+Values (2) ;
+
+Insert dbo.Customer
+      ( CustomerId)
+Values (3) ;
+
+Insert dbo.Customer
+      ( CustomerId)
+Values (4) ;
+
+Insert dbo.Customer
+      ( CustomerId)
+Values (5) ;
+
+Insert dbo.Customer
+      ( CustomerId)
+Values (6) ;
