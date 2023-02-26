627bb715ac676167e742e667ab993d81cd02ce43
Itsmetrunqhieu <69518893+itsmetrunqhieu@users.noreply.github.com>
2023-02-26 15:19:08 +0700

    FoodDeliveryApp

---
+++ Menu.sql
@@ -0,0 +1,9 @@
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (58 , 20, 'Soup', ' ', 19) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (59 , 20, 'Beef Tacos', ' ', 15) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (60 , 20, 'Pasta', ' ', 33) ;
+
