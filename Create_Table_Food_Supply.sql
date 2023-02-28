5168d6f4a047d05d6f1c948f899a9e19f93741b6
Nguyễn Đức Toàn <125717928+duwcston@users.noreply.github.com>
2023-02-28 15:07:18 +0700

    Update Create_Table_Food_Supply.sql
    update name in food supply

--- Create_Table_Food_Supply.sql
+++ Create_Table_Food_Supply.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Food_Supply (
 	Supply_Id	int			NOT NULL,
-	Name		int			NOT NULL,
+	Name		varchar(50)			NOT NULL,
 	Quantity	int			NOT NULL,
 	Date		datetime	NOT NULL,
 	Amount		int			NOT NULL,
