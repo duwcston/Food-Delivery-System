5d8a3116b681b8a8ecc4cc97a154ded6f17c3341
Nguyễn Đức Toàn <125717928+duwcston@users.noreply.github.com>
2023-02-28 14:40:53 +0700

    Create table + delivery data

---
+++ Create_Table_Food_Supply.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Food_Supply (
+	Supply_Id	int			NOT NULL,
+	Name		int			NOT NULL,
+	Quantity	int			NOT NULL,
+	Date		datetime	NOT NULL,
+	Amount		int			NOT NULL,
+	PRIMARY KEY (Supply_Id)
+	);
