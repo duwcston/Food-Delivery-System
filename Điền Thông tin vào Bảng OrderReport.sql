be70f449d55bf603fbfa48136c3dd300746560aa
Itsmetrunqhieu <69518893+itsmetrunqhieu@users.noreply.github.com>
2023-03-03 20:18:04 +0700

    update order report values

--- Điền Thông tin vào Bảng OrderReport.sql
+++ Điền Thông tin vào Bảng OrderReport.sql
@@ -21,3 +21,19 @@ Values (5 , 5, 5, 5) ;
 Insert dbo.OrderReport
       ( OrderId, CustomerId, FoodId, DeliveryId)
 Values (6 , 6, 6, 6) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (7 , 7, 7, 7) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (8 , 8, 8, 8) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (9 , 9, 9, 9) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (10 , 10, 10, 10) ;
