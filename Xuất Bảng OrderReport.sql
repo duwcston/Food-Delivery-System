3c89e17ef3061aaa4273f2084de4d78cf429a5d7
Nguyễn Đức Toàn <125717928+duwcston@users.noreply.github.com>
2023-03-19 15:17:48 +0700

    Delete Xuất Bảng OrderReport.sql

--- Xuất Bảng OrderReport.sql
+++
@@ -1,9 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [OrderId]
-      ,[CustomerId]
-      ,[FoodId]
-      ,[DeliveryId]
-      ,[Value_Status]
-      ,[Timeline]
-      ,[Quantity]
-  FROM [FoodDeliveryApp].[dbo].[OrderReport]
