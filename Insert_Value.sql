e9c8215d0c4ee440dcbee981e3b04b8f99fbec3d
Nguyễn Đức Toàn <125717928+duwcston@users.noreply.github.com>
2023-03-19 15:21:09 +0700

    Update Database

---
+++ Insert_Value.sql
@@ -0,0 +1,380 @@
+Insert Bill_reports ( Bill_ID,Cust_Id, Order_Id, Supply_Id ,Food_Id, Delivery_Id, Payment_Id, Date, Rating )
+Values (1,1,1,1,1,1,1,2023-02-14, 4);
+
+Insert Bill_reports ( Bill_ID,Cust_Id, Order_Id, Supply_Id ,Food_Id, Delivery_Id, Payment_Id, Date, Rating )
+Values (2,2,2,2,2,2,2,2023-02-21, 4);
+
+Insert Bill_reports ( Bill_ID,Cust_Id, Order_Id, Supply_Id ,Food_Id, Delivery_Id, Payment_Id, Date, Rating  )
+Values (3,3,3,3,3,3,3,2023-02-24, 4);
+
+Insert Bill_reports ( Bill_ID,Cust_Id, Order_Id, Supply_Id ,Food_Id, Delivery_Id, Payment_Id, Date, Rating  )
+Values (4,4,4,4,4,4,4,2023-02-14, 4);
+
+Insert Bill_reports ( Bill_ID,Cust_Id, Order_Id, Supply_Id ,Food_Id, Delivery_Id, Payment_Id, Date, Rating  )
+Values (5,5,5,5,5,5,5,2023-01-31, 4);
+
+Insert  Bill_reports ( Bill_ID,Cust_Id, Order_Id, Supply_Id ,Food_Id, Delivery_Id, Payment_Id, Date, Rating  )
+Values (6,6,6,6,6,6,6,2023-02-02, 4);
+
+Insert  Bill_reports ( Bill_ID,Cust_Id, Order_Id, Supply_Id ,Food_Id, Delivery_Id, Payment_Id, Date, Rating  )
+Values (7,7,7,7,7,7,7,2023-01-25, 4);
+
+Insert  Bill_reports ( Bill_ID,Cust_Id, Order_Id, Supply_Id ,Food_Id, Delivery_Id, Payment_Id, Date, Rating )
+Values (8,8,8,8,8,8,8,2023-01-10, 4);
+
+Insert Bill_reports ( Bill_ID,Cust_Id, Order_Id, Supply_Id ,Food_Id, Delivery_Id, Payment_Id, Date, Rating )
+Values (9,9,9,9,9,9,9,2023-01-06, 4);
+
+Insert  Bill_reports ( Bill_ID,Cust_Id, Order_Id, Supply_Id ,Food_Id, Delivery_Id, Payment_Id, Date, Rating )
+Values (10,10,10,10,10,10,10,2023-02-28, 4);
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (1 , 1, 'BBQ Ribs', ' ', 30) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (2 , 1, 'Lentil Soup', ' ', 28) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (3 , 1, 'Buffalo Wings', ' ', 29) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (4 , 2, 'Club Sandwiches', ' ', 7) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (5 , 2, 'Shrimp', ' ', 12) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (6 , 2, 'Ice Cream', ' ', 5) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (7 , 3, 'Basmati Rice', ' ', 12) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (8 , 3, 'BBQ Ribs', ' ', 31) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (9 , 3, 'Fish Sticks', ' ', 20) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (10 , 4, 'Greek Salad', ' ', 10) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (11 , 4, 'Shrimp', ' ', 12) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (12 , 4, 'Brown Rice', ' ', 14) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (13 , 5, 'Beef Tacos  ', ' ', 34) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (14 , 5, 'Jambalaya', ' ', 12) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (15 , 5, 'Brown Rice', ' ', 18) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (16 , 6, 'Donuts', ' ', 7) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (17 , 6, 'Grilled Cheese', ' ', 21) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (18 , 6, 'Popcorn', ' ', 11) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (19 , 7, 'Fish Sticks', ' ', 20) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (20 , 7, 'Grilled Cheese', ' ', 21) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (21 , 7, 'Club Sandwiches', ' ', 7) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (22 , 8, 'Onion Rings', ' ', 12) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (23 , 8, 'Hot Dogs', ' ', 12) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (24 , 8, 'Club Sandwiches', ' ', 7) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (25 , 9, 'Greek Salad', ' ', 11) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (26 , 9, 'Hot Dogs', ' ', 12) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (27 , 9, 'Club Sandwiches', ' ', 7) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (28 , 10, 'Buffalo Wings', ' ', 31) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (29 , 10, 'Beef Tacos', ' ', 22) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (30 , 10, 'Popcorn', ' ', 8) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (31 , 11, 'Basmati Rice', ' ', 21) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (32 , 11, 'Onion Rings', ' ', 14) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (33 , 11, 'Shrimp', ' ', 17) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (34 , 12, 'Meatballs', ' ', 19) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (35 , 12, 'Beef Tacos', ' ', 14) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (36 , 12, 'Bento Box', ' ', 20) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (37 , 13, 'Soup', ' ', 19) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (38 , 13, 'Beef Tacos', ' ', 32) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (39 , 13, 'Grilled Cheese', ' ', 42) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (40 , 14, 'Hot Dogs', ' ', 13) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (41 , 14, 'Beef Tacos', ' ', 23) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (42 , 14, 'Ice Cream', ' ', 8) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (43 , 15, 'Jambalaya', ' ', 13) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (44 , 15, 'Beef Tacos', ' ', 23) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (45 , 15, 'Brown Rice', ' ', 18) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (46 , 16, 'Chicken Sandwich', ' ', 13) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (47 , 16, 'Beef Tacos', ' ', 15) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (48 , 16, 'Lentil Soup', ' ', 10) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (49 , 17, 'Strawberries', ' ', 10) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (50 , 17, 'Spaghetti', ' ', 23) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (51 , 17, 'Pad Thai', ' ', 10) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (52 , 18, 'Bento Box', ' ', 19) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (53 , 18, 'Popcorn', ' ', 9) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (54 , 18, 'Chicken Sandwich', ' ', 26) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (55 , 19, 'Soup', ' ', 17) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (56 , 19, 'Fish Sticks', ' ', 14) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (57 , 19, 'Greek Salad', ' ', 23) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (58 , 20, 'Soup', ' ', 19) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (59 , 20, 'Beef Tacos', ' ', 15) ;
+
+Insert dbo.Menu ( Food_Id, Res_Id, Food_Name, Description_Info, Price)
+Values (60 , 20, 'Pasta', ' ', 33) ;
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (1,'wichcraft', '7238 River Street');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (2,'12 Chairs', '700 East Rocky River Ave');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (3,'5 Napkin Burger', '7590 Amerige Road Evans');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (4,'67 Burger', '842 Beaver Ridge');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (5,'Alidoro', '2 Homewood');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (6,'Amma', '58 Blackburn Street');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (7,'Amy Ruth', '53 North Rock Maple St');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (8,'Anjappar Chettinad', '80 Beaver Ridge');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (9,'Asuka Sushi', '8003 Fulton Avenue');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (10,'Aurora', '676 Hillside Ave');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (11,'Balade', '9065 Somerset Stree');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (12,'Balthazar Boulangerie', '220 Surrey Lane');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (13,'Barbounia', '9076 Fawn Ave');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (14,'Bareburger', '812 East Manhattan');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (15,'Benihana', '58 Prairie');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (16,'Bhatti Indian Grill', '350 South Brewery');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (17,'Big Daddy', '81 Alderwood Street');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (18,'Big Wong Restaurant', '602 Glen Eagles Street');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (19,'Bistango', '9562 Garfield Street');
+
+Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
+Values (20,'Blue Ribbon Brooklyn', '8582 Shadow Brook');
+
+Insert dbo.OrderReport 
+		( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (1 , 1, 1, 1) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (2 , 2, 2, 2) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (3 , 3, 3, 3) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (4 , 4, 4, 4) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (5 , 5, 5, 5) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (6 , 6, 6, 6) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (7 , 7, 7, 7) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (8 , 8, 8, 8) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (9 , 9, 9, 9) ;
+
+Insert dbo.OrderReport
+      ( Order_Id, Customer_Id, Food_Id, Delivery_Id)
+Values (10 , 10, 10, 10) ;
+
+Insert dbo.Customer
+      ( Customer_Id)
+Values (1) ;
+
+Insert dbo.Customer
+      ( Customer_Id)
+Values (2) ;
+
+Insert dbo.Customer
+      ( Customer_Id)
+Values (3) ;
+
+Insert dbo.Customer
+      ( Customer_Id)
+Values (4) ;
+
+Insert dbo.Customer
+      ( Customer_Id)
+Values (5) ;
+
+Insert dbo.Customer
+      ( Customer_Id)
+Values (6) ;
+
+Insert dbo.Customer
+      ( Customer_Id)
+Values (7) ;
+
+Insert dbo.Customer
+      ( Customer_Id)
+Values (8) ;
+
+Insert dbo.Customer
+      ( Customer_Id)
+Values (9) ;
+
+Insert dbo.Customer
+      ( Customer_Id)
+Values (10) ;
+
+Insert [dbo].[Payment] ( [Payment_id], [Order_id], [Type of payment])
+Values (1,1, 'Tien Mat');
+
+Insert [dbo].[Payment] ( [Payment_id], [Order_id], [Type of payment])
+Values (2,2, 'Online');
+
+Insert [dbo].[Payment] ( [Payment_id], [Order_id], [Type of payment])
+Values (3,3, 'Tien Mat');
+
+Insert [dbo].[Payment] ( [Payment_id], [Order_id], [Type of payment])
+Values (4,4, 'Tien Mat');
+
+Insert [dbo].[Payment] ( [Payment_id], [Order_id], [Type of payment])
+Values (5,5, 'Online');
+
+Insert [dbo].[Payment] ( [Payment_id], [Order_id], [Type of payment])
+Values (6,6, 'Tien Mat');
+
+Insert [dbo].[Payment] ( [Payment_id], [Order_id], [Type of payment])
+Values (7,7, 'Online');
+
+Insert [dbo].[Payment] ( [Payment_id], [Order_id], [Type of payment])
+Values (8,8, 'Tien Mat');
+
+Insert [dbo].[Payment] ( [Payment_id], [Order_id], [Type of payment])
+Values (9,9, 'Tien Mat');
+
+Insert [dbo].[Payment] ( [Payment_id], [Order_id], [Type of payment])
+Values (10,10, 'Online');
+
