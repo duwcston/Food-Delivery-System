9dc0d046a2c83a26fff6138ca9aa49a616e8ffa1
Nguyễn Đức Toàn <125717928+duwcston@users.noreply.github.com>
2023-03-19 15:15:02 +0700

    Delete Create_Table_Delivery.sql

--- Create_Table_Delivery.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Delivery (
-	Delivery_Id	int			NOT NULL,
-	Cust_Id		int			NOT NULL,
-	Food_Id		int			NOT NULL,
-	Quantity	int			NOT NULL,
-	Date		datetime	NOT NULL,
-	PRIMARY KEY (Delivery_Id)
-	);
