92a0df00a1e6ab561bd81c859032a8445fddab55
Nguyễn Đức Toàn <125717928+duwcston@users.noreply.github.com>
2023-03-19 15:17:42 +0700

    Delete Xuất Bảng Customer.sql

--- Xuất Bảng Customer.sql
+++
@@ -1,7 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [CustomerId]
-      ,[CustomerName]
-      ,[Contact_Num]
-      ,[CustomerAddress]
-      ,[Customer_money_account]
-  FROM [FoodDeliveryApp].[dbo].[Customer]
