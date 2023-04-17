8268653e50f7997ba19fae0445f09f23c31f83e1
Nguyen Duc Toan <125717928+duwcston@users.noreply.github.com>
2023-04-17 23:25:37 +0700

    Update Data in OrderReport

--- Insert_Value.sql
+++ Insert_Value.sql
@@ -378,3 +378,16 @@ Values (9,9, 'Tien Mat');
 Insert [dbo].[Payment] ( [Payment_id], [Order_id], [Type of payment])
 Values (10,10, 'Online');
 
+Insert dbo.OrderReport
+         ( OrderId, CustomerId, FoodId, DeliveryId, Quantity,Value_Status,Timeline)
+Values   (1 , 1, 1, 1, 1,'Dang giao', '2023-04-01' ),
+         (2, 2, 2, 2, 2, 'Dang nau','2023-04-30' ),
+	 (3, 3, 3, 3, 3, 'Dang giao','2023-04-19' ),
+	 (4,4,4,4,4,'Dang nhan don','2023-04-05' ),
+	 (5,5,5,5,3, 'Dang nau','2023-04-23' ),
+	 (6,6,6,6,2, 'Dang nhan don','2023-04-31' ),
+	 (7,7,7,7,1, ' Dang nau', '2023-04-15'),
+	 (8,8,8,8,4, 'Dang nhan don', '2023-04-05'),
+	 (9,9,9,9,3, 'Dang giao','2023-02-06' ),
+	 (10,10,10,10,2, 'Dang giao', '2023-03-07')
+
