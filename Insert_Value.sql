f01fe4fad48f32876e4e301c8f3909a483766a1c
Nguyen Duc Toan <125717928+duwcston@users.noreply.github.com>
2023-04-17 23:26:42 +0700

    Update Data in Customer and OrderReport

--- Insert_Value.sql
+++ Insert_Value.sql
@@ -390,4 +390,19 @@ Values   (1 , 1, 1, 1, 1,'Dang giao', '2023-04-01' ),
 	 (8,8,8,8,4, 'Dang nhan don', '2023-04-05'),
 	 (9,9,9,9,3, 'Dang giao','2023-02-06' ),
 	 (10,10,10,10,2, 'Dang giao', '2023-03-07')
+	 
+Insert into dbo.Customer
+        ( CustomerId, CustormerName, Contact_Num, CustomerAddress, Customer_money_account)
+ Values (1, 'Sylvia Villanueva', '209-200-7592','961 Katlyn Estates','GB31NGHZ43091700774225'),
+	(2, 'Jalen Pineda', '209-201-5657','547 Katrine Villages','GB71XEHT18064203044383'),
+	(3, 'Brady Nelson','209-202-2321','231 Block Loaf','GB58QVCW30321303860011'),
+	(4, 'Junior Wade', '209-204-3508','30671 Vivianne Landing','GB09AUGN01620186042022'),
+	(5, ' Julius Daugherty','209-205-9860','13069 Corwin Gateway','GB67XQDD10718250592126'),
+	(6,'Steven Abbott', '209-206-4172','993 Julio Lake','GB38OWBH30076390085640'),
+	(7,'Derek Horn', '209-207-7928', '883 Barton Square','GB87USFA10022780808873'),
+	(8, 'Rhett Quinn', '209-208-6614','565 Balistreri Station','GB76NOHP20900116002068'),
+	(9, 'Anabella Black', '209-210-1167','04306 Nat Ridge','GB60KFXF88200280205417'),
+	(10, ' Sidney Gentry', '209-212-7150','29603 Bosco Islands','GB87WBUM72782330380604')
+
+
 
