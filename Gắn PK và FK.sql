89778457179eb05ecff2c25a91185822a2eee1ce
Đoàn Nhân Hậu <125809343+Tobi3007@users.noreply.github.com>
2023-04-18 23:16:18 +0700

    Update Gắn PK và FK.sql

--- Gắn PK và FK.sql
+++ Gắn PK và FK.sql
@@ -1,6 +1,2 @@
 ALTER TABLE dbo.Customer
 ADD PRIMARY KEY (CustomerId);
-ALTER TABLE dbo.OrderReport
-ADD PRIMARY KEY (OrderId);
-ALTER TABLE dbo.OrderReport
-ADD FOREIGN KEY (CustomerId) REFERENCES dbo.Customer(CustomerID);
