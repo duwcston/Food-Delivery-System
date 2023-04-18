70f8d12633e5cdc1219b276228ab966dd597578b
Đoàn Nhân Hậu <125809343+Tobi3007@users.noreply.github.com>
2023-04-18 23:13:33 +0700

    Update Tạo Database và tạo Bảng.sql

--- Tạo Database và tạo Bảng.sql
+++ Tạo Database và tạo Bảng.sql
@@ -8,17 +8,8 @@ Go
 		  CustomerName nvarchar(50) ,
 		  Contact_Num nvarchar(50) ,
 		  CustomerAddress nvarchar(50),
-		  Customer_money_account nvarchar(50)
+		  Email nvarchar(50),
+		  Password nvarchar(50)
 	  )
 	  Go
-	  Create Table OrderReport
-	  (  
-	      OrderId Int Unique Not Null,
-		  CustomerId int Not Null,
-		  FoodId int ,
-		  DeliveryId int ,
-		  Value_Status nvarchar(20) ,
-		  Timeline Datetime,
-		  Quantity Int
-	  )
-	  Go
+	
