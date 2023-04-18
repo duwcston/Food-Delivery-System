267ed2c5d56bac733644d82494a0033cc8281180
Đoàn Nhân Hậu <125809343+Tobi3007@users.noreply.github.com>
2023-04-18 23:15:17 +0700

    Update Điền thông tin vào Bảng Customer.sql

--- Điền thông tin vào Bảng Customer.sql
+++ Điền thông tin vào Bảng Customer.sql
@@ -1,14 +1,16 @@
 Insert into dbo.Customer
-        ( CustomerId, CustormerName, Contact_Num, CustomerAddress, Customer_money_account)
- Values (1, 'Sylvia Villanueva', '209-200-7592','961 Katlyn Estates','GB31NGHZ43091700774225'),
-	(2, 'Jalen Pineda', '209-201-5657','547 Katrine Villages','GB71XEHT18064203044383'),
-	(3, 'Brady Nelson','209-202-2321','231 Block Loaf','GB58QVCW30321303860011'),
-	(4, 'Junior Wade', '209-204-3508','30671 Vivianne Landing','GB09AUGN01620186042022'),
-	(5, ' Julius Daugherty','209-205-9860','13069 Corwin Gateway','GB67XQDD10718250592126'),
-	(6,'Steven Abbott', '209-206-4172','993 Julio Lake','GB38OWBH30076390085640'),
-	(7,'Derek Horn', '209-207-7928', '883 Barton Square','GB87USFA10022780808873'),
-	(8, 'Rhett Quinn', '209-208-6614','565 Balistreri Station','GB76NOHP20900116002068'),
-	(9, 'Anabella Black', '209-210-1167','04306 Nat Ridge','GB60KFXF88200280205417'),
-	(10, ' Sidney Gentry', '209-212-7150','29603 Bosco Islands','GB87WBUM72782330380604')
+        ( CustomerId, CustormerName, Contact_Num, CustomerAddress, Email, Cust_Password))
+ Values (3165, 'Sylvia Villanueva', '209-200-7592','961 Katlyn Estates','zirutolexo-2470@yopmail.com','xMlda3y7gB'),
+	(5330, 'Jalen Pineda', '209-201-5657','547 Katrine Villages','cruxeikeprici-4266@yopmail.com','0jlJaAg52N'),
+	(2177, 'Brady Nelson','209-202-2321','231 Block Loaf','hittesitricru-1251@yopmail.com','yCxfjGP7MB'),
+	(4778, 'Junior Wade', '209-204-3508','30671 Vivianne Landing','baudibranonni-4505@yopmail.com','upxQku0xCg'),
+	(2180, 'Julius Daugherty','209-205-9860','13069 Corwin Gateway','teittigroimmaupau-6138@yopmail.com','4lxQMc57QP'),
+	(6333, 'Steven Abbott', '209-206-4172','993 Julio Lake','napegrezeka-5706@yopmail.com','6MFwpoGmBg'),
+	(4085, 'Derek Horn', '209-207-7928', '883 Barton Square','ramoffehuya-7102@yopmail.com','z3ZjBbqRnQ'),
+	(8935, 'Rhett Quinn', '209-208-6614','565 Balistreri Station','facojellemau-8974@yopmail.com','XzCvXI7gCO'),
+	(9215, 'Anabella Black', '209-210-1167','04306 Nat Ridge','woprijawipra-9522@yopmail.com','MzmJ4Zojt6'),
+	(1080, 'Sidney Gentry', '209-212-7150','29603 Bosco Islands','peddewenubo-3955@yopmail.com','5MkQhTg2nf')
+
+
 
 
