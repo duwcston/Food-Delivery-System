ea9d0e0a6b35a6faafbf6462764f11e9b6e3eccd
Nguyen Duc Toan <125717928+duwcston@users.noreply.github.com>
2023-04-20 15:19:13 +0700

    Update FK and PK

--- Create_FDA_Table.sql
+++ Create_FDA_Table.sql
@@ -1,46 +1,50 @@
 CREATE TABLE Customer (  
-	Cus_ID 				Int	Unique		NOT NULL,
-	Cus_name 			nvarchar(30)	NOT NULL,
-	Cus_phone_num	 	Int	Unique		NOT NULL,
-	Cus_address 		varchar(30)		NOT NULL,
-	Cus_email			varchar(20)		Unique,
-	Cus_password		varchar(15)		NOT NULL,
+	Cus_ID 				Int		Unique		NOT NULL,
+	Cus_name 			nvarchar(30)			NOT NULL,
+	Cus_phone_num	 		Int		Unique		NOT NULL,
+	Cus_address 			varchar(30)			NOT NULL,
+	Cus_email			varchar(20)	Unique,
+	Cus_password			varchar(15)			NOT NULL,
 	PRIMARY KEY (Cus_ID)
 	  );
 
 CREATE TABLE Restaurant (  
-	Res_ID 				varchar(30) Unique 		NOT NULL,
+	Res_ID 				varchar(30) 	Unique 		NOT NULL,
 	Res_name 			nvarchar(30),
 	Res_address			varchar(30),
 	PRIMARY KEY (Res_ID)
-	  );
-
-CREATE TABLE Menu (  
-	Food_ID				Int	Unique		NOT NULL,
-	Res_ID				varchar(30)		NOT NULL,
-	Menu_ID				varchar(30)		NOT NULL,
-	PRIMARY KEY (Menu_ID)	s
 	  );
 
 CREATE TABLE Food (
 	Cus_ID				Int				NOT NULL,
 	Food_ID				Int				NOT NULL,
 	Description			varchar(50),
-	Price$				Int				NOT NULL,
+	Price__$			Int				NOT NULL,
 	Rating				Int,
 	CHECK(Rating >= 1),
 	CHECK(Rating <= 5),
-	PRIMARY KEY (Food_ID)
+	PRIMARY KEY (Food_ID),
+	FOREIGN KEY (Cus_ID) REFERENCES Customer(Cus_ID)
 	);
+
+CREATE TABLE Menu (  
+	Food_ID				Int		Unique		NOT NULL,
+	Res_ID				varchar(30)			NOT NULL,
+	Menu_ID				varchar(30)			NOT NULL,
+	PRIMARY KEY (Menu_ID),
+	FOREIGN KEY (Food_ID) REFERENCES Food(Food_ID),
+	FOREIGN KEY (Res_ID) REFERENCES Restaurant(Res_ID)
+	  );
 
 CREATE TABLE Payment (
 	Food_ID				Int				NOT NULL,
-	Payment_ID			Int 			NOT NULL,
-	Amount			 	Int 			NOT NULL,
-	Total$				Int 			NOT NULL,
+	Payment_ID			Int 				NOT NULL,
+	Amount			 	Int 				NOT NULL,
+	Total_$				Int 				NOT NULL,
 	Method				Int				NOT NULL,
 	/*0: cash, 1: credit*/
-	PRIMARY KEY (Payment_ID)
+	PRIMARY KEY (Payment_ID),
+	FOREIGN KEY (Food_ID) REFERENCES Food(Food_ID)
 	);	
 
 CREATE TABLE Bill_reports (
@@ -48,15 +52,20 @@ CREATE TABLE Bill_reports (
 	Cus_ID				Int				NOT NULL,
 	Food_ID				Int				NOT NULL,
 	Payment_ID			Int				NOT NULL,
-	Order_time			datetime		NOT NULL,
-	PRIMARY KEY (Bill_ID)
+	Order_time			datetime			NOT NULL,
+	PRIMARY KEY (Bill_ID),
+	FOREIGN KEY (Cus_ID) REFERENCES Customer(Cus_ID),
+	FOREIGN KEY (Food_ID) REFERENCES Food(Food_ID),
+	FOREIGN KEY (Payment_ID) REFERENCES Payment(Payment_ID)
 	);
 
 CREATE TABLE Delivery_center (
 	Cus_ID				Int				NOT NULL,
 	Bill_ID				Int				NOT NULL,
 	Shipper_ID			Int				NOT NULL,
-	Shipper_name		varchar(30)		NOT NULL,
-	Shipper_phone_num	Int				NOT NULL,
-	PRIMARY KEY (Shipper_ID)
+	Shipper_name			varchar(30)	 unique		NOT NULL,
+	Shipper_phone_num		Int		 unique		NOT NULL,
+	PRIMARY KEY (Shipper_ID),
+	FOREIGN KEY (Cus_ID) REFERENCES Customer(Cus_ID),
+	FOREIGN KEY (Bill_ID) REFERENCES Bill_reports(Bill_ID)
 	);	
