25d45fe6bea2f6cbc048ee7b046c5386831baa2b
Itsmetrunqhieu <69518893+itsmetrunqhieu@users.noreply.github.com>
2023-04-22 11:13:47 +0700

    update restaurant_list
    newest restaurant_list on 22/4/2023

--- Restaurant_insert_values.sql
+++ Restaurant_insert_values.sql
@@ -1,60 +1,31 @@
-Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (1,'wichcraft', '7238 River Street');
+Use FoodDelivery
 
 Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (2,'12 Chairs', '700 East Rocky River Ave');
+Values ('A3112','Paris meals', '7238 River Street');
 
 Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (3,'5 Napkin Burger', '7590 Amerige Road Evans');
+Values ('H92N','The Jazz Shark', '700 East Rocky River Ave');
 
 Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (4,'67 Burger', '842 Beaver Ridge');
+Values ('231M','Vegany', '7590 Amerige Road Evans');
 
 Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (5,'Alidoro', '2 Homewood');
+Values ('D9C3','Ivory', '842 Beaver Ridge');
 
 Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (6,'Amma', '58 Blackburn Street');
+Values ('U21U','The Mad Nights', '2 Homewood');
 
 Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (7,'Amy Ruth', '53 North Rock Maple St');
+Values ('O23V','Luxurian meat', '58 Blackburn Street');
 
 Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (8,'Anjappar Chettinad', '80 Beaver Ridge');
+Values ('41OA','bee pancake', '53 North Rock Maple St');
 
 Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (9,'Asuka Sushi', '8003 Fulton Avenue');
+Values ('525P','poland spaget', '80 Beaver Ridge');
 
 Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (10,'Aurora', '676 Hillside Ave');
+Values ('P41F','Asuka Sushi', '8003 Fulton Avenue');
 
 Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (11,'Balade', '9065 Somerset Stree');
-
-Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (12,'Balthazar Boulangerie', '220 Surrey Lane');
-
-Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (13,'Barbounia', '9076 Fawn Ave');
-
-Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (14,'Bareburger', '812 East Manhattan');
-
-Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (15,'Benihana', '58 Prairie');
-
-Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (16,'Bhatti Indian Grill', '350 South Brewery');
-
-Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (17,'Big Daddy', '81 Alderwood Street');
-
-Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (18,'Big Wong Restaurant', '602 Glen Eagles Street');
-
-Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (19,'Bistango', '9562 Garfield Street');
-
-Insert dbo.Restaurant ( Res_ID, Res_Name, Res_Address)
-Values (20,'Blue Ribbon Brooklyn', '8582 Shadow Brook');
-
+Values ('L0G7','Nirvana', '676 Hillside Ave');
