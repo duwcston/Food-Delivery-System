4554015c70063a3bcac031e89d8e5dcc3e50392c
Đoàn Nhân Hậu <125809343+Tobi3007@users.noreply.github.com>
2023-04-24 23:42:44 +0700

    Update Điền thông tin vào Bảng Customer.sql

--- Điền thông tin vào Bảng Customer.sql
+++ Điền thông tin vào Bảng Customer.sql
@@ -1,15 +1,17 @@
 Insert into dbo.Customer
         ( CustomerId, CustormerName, Contact_Num, CustomerAddress, Email, Cust_Password))
- Values (3165, 'Sylvia Villanueva', '209-200-7592','961 Katlyn Estates','zirutolexo-2470@yopmail.com','xMlda3y7gB'),
-	(5330, 'Jalen Pineda', '209-201-5657','547 Katrine Villages','cruxeikeprici-4266@yopmail.com','0jlJaAg52N'),
-	(2177, 'Brady Nelson','209-202-2321','231 Block Loaf','hittesitricru-1251@yopmail.com','yCxfjGP7MB'),
-	(4778, 'Junior Wade', '209-204-3508','30671 Vivianne Landing','baudibranonni-4505@yopmail.com','upxQku0xCg'),
-	(2180, 'Julius Daugherty','209-205-9860','13069 Corwin Gateway','teittigroimmaupau-6138@yopmail.com','4lxQMc57QP'),
-	(6333, 'Steven Abbott', '209-206-4172','993 Julio Lake','napegrezeka-5706@yopmail.com','6MFwpoGmBg'),
-	(4085, 'Derek Horn', '209-207-7928', '883 Barton Square','ramoffehuya-7102@yopmail.com','z3ZjBbqRnQ'),
-	(8935, 'Rhett Quinn', '209-208-6614','565 Balistreri Station','facojellemau-8974@yopmail.com','XzCvXI7gCO'),
-	(9215, 'Anabella Black', '209-210-1167','04306 Nat Ridge','woprijawipra-9522@yopmail.com','MzmJ4Zojt6'),
-	(1080, 'Sidney Gentry', '209-212-7150','29603 Bosco Islands','peddewenubo-3955@yopmail.com','5MkQhTg2nf')
+ Values ('D1W12', 'Sylvia Villanueva', '209-200-7592','961 Katlyn Estates','zirutolexo-2470@yopmail.com','xMlda3y7gB'),
+	('H13AS7', 'Jalen Pineda', '209-201-5657','547 Katrine Villages','cruxeikeprici-4266@yopmail.com','0jlJaAg52N'),
+	('O141S1', 'Brady Nelson','209-202-2321','231 Block Loaf','hittesitricru-1251@yopmail.com','yCxfjGP7MB'),
+	('P51T31', 'Junior Wade', '209-204-3508','30671 Vivianne Landing','baudibranonni-4505@yopmail.com','upxQku0xCg'),
+	('N13R4T', 'Julius Daugherty','209-205-9860','13069 Corwin Gateway','teittigroimmaupau-6138@yopmail.com','4lxQMc57QP'),
+	('M4D1R', 'Steven Abbott', '209-206-4172','993 Julio Lake','napegrezeka-5706@yopmail.com','6MFwpoGmBg'),
+	('U134D', 'Derek Horn', '209-207-7928', '883 Barton Square','ramoffehuya-7102@yopmail.com','z3ZjBbqRnQ'),
+	('J2D24', 'Rhett Quinn', '209-208-6614','565 Balistreri Station','facojellemau-8974@yopmail.com','XzCvXI7gCO'),
+	('Q31H3', 'Anabella Black', '209-210-1167','04306 Nat Ridge','woprijawipra-9522@yopmail.com','MzmJ4Zojt6'),
+	('K14F2', 'Sidney Gentry', '209-212-7150','29603 Bosco Islands','peddewenubo-3955@yopmail.com','5MkQhTg2nf'),
+	('513EB', 'Miki Harada', '207-997-5438','643 Bicetown Road','cretaureicreippu-2696@yopmail.com','QfeHua00Ho'),
+	('L41FB1', 'Yasuo Yamada', '202-579-9571','1110 Small Street','frijeubiziquoi-3024@yopmail.com','QL3tfB18Wl')
 
 
 
