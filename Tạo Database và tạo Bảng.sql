aebe3775021b5f989aa361e34e366f7e2ac8e13c
Đoàn Nhân Hậu <125809343+Tobi3007@users.noreply.github.com>
2023-04-30 22:32:06 +0700

    Update Tạo Database và tạo Bảng.sql

--- Tạo Database và tạo Bảng.sql
+++ Tạo Database và tạo Bảng.sql
@@ -6,9 +6,9 @@ Go
 	  (  
 	      C_ID nvarchar(50) Unique Not Null,
 		  C_Name nvarchar(50) ,
-		  Phone_Num nvarchar(50) ,
-		  Address nvarchar(50),
-		  Email nvarchar(50),
+		  C_Phone nvarchar(50) ,
+		  C_Address nvarchar(50),
+		  C_Email nvarchar(50),
 		  C_Password nvarchar(50)
 	  )
 	  Go
