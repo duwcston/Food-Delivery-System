32c48330b4e1e58383cd396347ecf79cc0826b1e
Đoàn Nhân Hậu <125809343+Tobi3007@users.noreply.github.com>
2023-04-30 22:27:31 +0700

    Update Tạo Database và tạo Bảng.sql

--- Tạo Database và tạo Bảng.sql
+++ Tạo Database và tạo Bảng.sql
@@ -4,12 +4,12 @@ Go
    Go
       Create Table Customer
 	  (  
-	      CustomerId nvarchar(50) Unique Not Null,
-		  CustomerName nvarchar(50) ,
-		  Contact_Num nvarchar(50) ,
-		  CustomerAddress nvarchar(50),
+	      C_ID nvarchar(50) Unique Not Null,
+		  C_Name nvarchar(50) ,
+		  Phone_Num nvarchar(50) ,
+		  Address nvarchar(50),
 		  Email nvarchar(50),
-		  Cust_Password nvarchar(50)
+		  Password nvarchar(50)
 	  )
 	  Go
 	
